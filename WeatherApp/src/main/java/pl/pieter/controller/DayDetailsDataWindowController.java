788670367dd041b7bd2252474e2886f05a3e5f31
Piotr Kycia <pieterkycia@gmail.com>
2022-08-25 13:21:43 +0200

    Created DayDetailsDataWindowController class

---
+++ WeatherApp/src/main/java/pl/pieter/controller/DayDetailsDataWindowController.java
@@ -0,0 +1,83 @@
+package pl.pieter.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
+import pl.pieter.view.ViewManager;
+import pl.pieter.weather.library.DailyData;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DayDetailsDataWindowController extends BaseController {
+
+    private DailyData.DayData dayData;
+
+    public DayDetailsDataWindowController(ViewManager viewManager, String fxmlPath, int index) {
+        super(viewManager, fxmlPath);
+        this.dayData = viewManager.getWeatherManager().getWeatherDataModelFx().getDailyDataModelFx().getDailyData().getDayDataList().get(index);
+    }
+
+    @FXML
+    private Label humidityLabel;
+
+    @FXML
+    private Label maxTempLabel;
+
+    @FXML
+    private Label minTempLabel;
+
+    @FXML
+    private ProgressIndicator moonphaseProgressIndicator;
+
+    @FXML
+    private Label moonriseLabel;
+
+    @FXML
+    private Label moonsetLabel;
+
+    @FXML
+    private Label rainLabel;
+
+    @FXML
+    private Label sunriseLabel;
+
+    @FXML
+    private Label sunsetLabel;
+
+    @FXML
+    private Label uvIndexLabel;
+
+    @FXML
+    private Label windSpeedLabel;
+
+    @FXML
+    private VBox vBoxData;
+
+
+    @FXML
+    public void initialize() {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+
+        maxTempLabel.setText(String.valueOf(dayData.getTemp().getMax()) + " C");
+        minTempLabel.setText(String.valueOf(dayData.getTemp().getMin()) + " C");
+
+
+        sunriseLabel.setText(simpleDateFormat.format(new Date(dayData.getSunrise() * 1000)));
+        sunsetLabel.setText(simpleDateFormat.format(new Date(dayData.getSunset() * 1000)));
+
+        moonriseLabel.setText(simpleDateFormat.format(new Date(dayData.getMoonrise() * 1000)));
+        moonsetLabel.setText(simpleDateFormat.format(new Date(dayData.getMoonset() * 1000)));
+        moonphaseProgressIndicator.setProgress(dayData.getMoonPhase());
+
+        rainLabel.setText(String.valueOf(dayData.getPop() * 100) + " %");
+
+        uvIndexLabel.setText(String.valueOf(dayData.getUvi()));
+
+        humidityLabel.setText(String.valueOf(dayData.getHumidity()) + " %");
+
+        windSpeedLabel.setText(String.valueOf(dayData.getWindSpeed()) + " m/s");
+    }
+}
