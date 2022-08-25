d9446306de1fde664be9e92be9e5e16bf155a712
Piotr Kycia <pieterkycia@gmail.com>
2022-08-25 13:32:08 +0200

    Created EmptyWindowController class

---
+++ WeatherApp/src/main/java/pl/pieter/controller/EmptyWindowController.java
@@ -0,0 +1,10 @@
+package pl.pieter.controller;
+
+import pl.pieter.view.ViewManager;
+
+public class EmptyWindowController extends BaseController {
+
+    public EmptyWindowController(ViewManager viewManager, String fxmlPath) {
+        super(viewManager, fxmlPath);
+    }
+}
