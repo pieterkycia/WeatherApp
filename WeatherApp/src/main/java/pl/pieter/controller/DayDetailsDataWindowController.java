cb07a20d085e0ca9822f3eb5717b32dc196f83ad
Piotr Kycia <pieterkycia@gmail.com>
2022-09-30 13:47:00 +0200

    Added moon-phase icons

--- WeatherApp/src/main/java/pl/pieter/controller/DayDetailsDataWindowController.java
+++ WeatherApp/src/main/java/pl/pieter/controller/DayDetailsDataWindowController.java
@@ -1,9 +1,19 @@
 package pl.pieter.controller;
 
+import de.jensd.fx.glyphs.GlyphsDude;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcons;
+import de.jensd.fx.glyphs.weathericons.WeatherIcons;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import pl.pieter.view.ViewManager;
 import pl.pieter.weather.library.DailyData;
 
@@ -29,7 +39,7 @@ public class DayDetailsDataWindowController extends BaseController {
     private Label minTempLabel;
 
     @FXML
-    private ProgressIndicator moonphaseProgressIndicator;
+    private VBox moonPhaseVBox;
 
     @FXML
     private Label moonriseLabel;
@@ -71,7 +81,7 @@ public class DayDetailsDataWindowController extends BaseController {
 
         moonriseLabel.setText(simpleDateFormat.format(new Date(dayData.getMoonrise() * 1000)));
         moonsetLabel.setText(simpleDateFormat.format(new Date(dayData.getMoonset() * 1000)));
-        moonphaseProgressIndicator.setProgress(dayData.getMoonPhase());
+        moonPhaseVBox.getChildren().add(setMoonPhase());
 
         rainLabel.setText(String.valueOf(Math.round(dayData.getPop() * 100)) + " %");
 
@@ -80,5 +90,76 @@ public class DayDetailsDataWindowController extends BaseController {
         humidityLabel.setText(String.valueOf(dayData.getHumidity()) + " %");
 
         windSpeedLabel.setText(String.valueOf(Math.round(dayData.getWindSpeed()) + " m/s"));
+    }
+
+    private VBox setMoonPhase() {
+        double step = 100.0 / (MoonPhase.values().length - 1);
+        int moonPhase = (int) Math.round((dayData.getMoonPhase() * 100) / step);
+
+        VBox moonIconVBox = new VBox(MoonPhase.values()[moonPhase].getIcon());
+
+        Label description = new Label(MoonPhase.values()[moonPhase].getDescription());
+        description.setTextFill(Paint.valueOf("#FFFFFF"));
+        description.setFont(Font.font(14));
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(moonIconVBox, description);
+
+        return vBox;
+    }
+
+    private enum MoonPhase {
+        MOON_NEW("Nów", GlyphsDude.createIcon(WeatherIcons.MOON_NEW, "30px")),
+        MOON_WAXING_CRESCENT_1("Przychadzący sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_CRESENT_1, "30px")),
+        MOON_WAXING_CRESCENT_2("Przychadzący sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_CRESENT_2, "30px")),
+        MOON_WAXING_CRESCENT_3("Przychadzący sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_CRESENT_3, "30px")),
+        MOON_WAXING_CRESCENT_4("Przychadzący sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_CRESENT_4, "30px")),
+        MOON_WAXING_CRESCENT_5("Przychadzący sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_CRESENT_5, "30px")),
+        MOON_WAXING_CRESCENT_6("Przychadzący sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_CRESENT_6, "30px")),
+
+        MOON_FIRST_QUARTER("Pierwsza kwadra", GlyphsDude.createIcon(WeatherIcons.MOON_FIRST_QUARTER, "30px")),
+
+        MOON_WAXING_GIBBOUS_1("Przychodzący garb", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_GIBBOUS_1, "30px")),
+        MOON_WAXING_GIBBOUS_2("Przychodzący garb", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_GIBBOUS_1, "30px")),
+        MOON_WAXING_GIBBOUS_3("Przychodzący garb", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_GIBBOUS_1, "30px")),
+        MOON_WAXING_GIBBOUS_4("Przychodzący garb", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_GIBBOUS_1, "30px")),
+        MOON_WAXING_GIBBOUS_5("Przychodzący garb", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_GIBBOUS_1, "30px")),
+        MOON_WAXING_GIBBOUS_6("Przychodzący garb", GlyphsDude.createIcon(WeatherIcons.MOON_WAXING_GIBBOUS_1, "30px")),
+
+        MOON_FULL("Pełnia", GlyphsDude.createIcon(WeatherIcons.MOON_FULL, "30px")),
+
+        MOON_WANING_GIBBOUS_1("Zanikający garb", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_GIBBOUS_1, "30px")),
+        MOON_WANING_GIBBOUS_2("Zanikający garb", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_GIBBOUS_1, "30px")),
+        MOON_WANING_GIBBOUS_3("Zanikający garb", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_GIBBOUS_1, "30px")),
+        MOON_WANING_GIBBOUS_4("Zanikający garb", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_GIBBOUS_1, "30px")),
+        MOON_WANING_GIBBOUS_5("Zanikający garb", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_GIBBOUS_1, "30px")),
+        MOON_WANING_GIBBOUS_6("Zanikający garb", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_GIBBOUS_1, "30px")),
+
+        MOON_3RD_QUARTER("Trzecia kwadra", GlyphsDude.createIcon(WeatherIcons.MOON_3RD_QUARTER, "30px")),
+
+        MOON_WANING_CRESCENT_1("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_CRESCENT_1, "30px")),
+        MOON_WANING_CRESCENT_2("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_CRESCENT_1, "30px")),
+        MOON_WANING_CRESCENT_3("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_CRESCENT_1, "30px")),
+        MOON_WANING_CRESCENT_4("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_CRESCENT_1, "30px")),
+        MOON_WANING_CRESCENT_5("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_CRESCENT_1, "30px")),
+        MOON_WANING_CRESCENT_6("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_WANING_CRESCENT_1, "30px")),
+
+        MOON_NEW_2("Zanikający sierp", GlyphsDude.createIcon(WeatherIcons.MOON_NEW, "30px"));
+
+        private String description;
+        private Text icon;
+
+        MoonPhase(String description, Text icon) {
+            this.description = description;
+            this.icon = icon;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Text getIcon() {
+            return icon;
+        }
     }
 }
